e1d5d57f10b6dcdcb91a80e9f24bdcf0702618ed
rubsanmon22 <ruben.sandez@unc.edu.com>
2025-09-04 17:48:32 -0300

    gcl funcional

--- tp1/rubfx.py
+++ tp1/rubfx.py
@@ -1,13 +1,20 @@
 #%%
-#Generador lineal de congruencia
-def gcl (x0, a, c, m, n):
+import numpy as np 
+#%%
+#Generador lineal de congruencia  (enteros)
+def gclz (x0, a, c, m, n):
 
     x = x0
 
     numeros = []
     for i in range(n):
         x = (a * x + c) % m
-        numeros.append(x / m)
-        
+        numeros.append(x)
+
     return numeros
+# %%
+#Generador lineal de congruencia
+def gclr (x0, a, c, m, n):
+
+    return np.array(gclz(x0, a, c, m, n)) / m
 # %%
