07ca676e1fd9b67893ca4bd02935da104f132553
rubsanmon22 <ruben.sandez@unc.edu.com>
2025-09-06 23:19:55 -0300

    actualización de versión

--- tp1/rubfx.py
+++ tp1/rubfx.py
@@ -1,7 +1,9 @@
 #%%
 import numpy as np 
 #%%
+
 #Generador lineal de congruencia  (enteros)
+
 def gclz (x0, a, c, m, n):
 
     x = x0
@@ -13,8 +15,23 @@ def gclz (x0, a, c, m, n):
 
     return numeros
 # %%
-#Generador lineal de congruencia
+
+#Generador lineal de congruencia (0,1)
+
 def gclr (x0, a, c, m, n):
 
     return np.array(gclz(x0, a, c, m, n)) / m
+
 # %%
+
+#Momentos teóricos de la distribución uniforme continua en [0,1]
+
+def momt (k):
+    return 1/(k+1)
+# %%
+
+#Calcula los momentos empíricos de la distribución obtenida
+
+def mome (data, k):
+    n = len(data) # Número de datos
+    return sum(x**k for x in data) / n
